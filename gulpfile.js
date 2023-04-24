ff54ddbb245d0c8b04098491bab6a0978c6de40d
aylikm <pf205japtmw@gmail.com>
2023-04-24 12:52:36 +0900

    first

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const ejs = require("gulp-ejs");
+const rename = require("gulp-rename");
+const browserSync = require('browser-sync');
+const babel = require('gulp-babel')
+
+const browserSyncFunc = () => {
+  browserSync.init({
+    server: 'dist/',
+    open: false
+  })
+}
+const browserSyncReload = (done) => {
+  browserSync.reload();
+  done();
+}
+
+const compJs = () => {
+  return gulp.src('src/js/*.js')
+  .pipe(babel(
+    {
+      presets: ['@babel/env', 'minify']
+    }
+  ))
+  .pipe(gulp.dest('dist/js'))
+}
+const sassComp = () => {
+  return gulp.src( 'src/sass/*.scss' )
+    .pipe( sass({outputStyle: 'compressed'}).on( 'error', sass.logError ) )
+    .pipe( autoprefixer())
+    .pipe( gulp.dest( './dist/css' ));
+}
+// const indexEjs = () => {
+//   return gulp.src(["src/ejs/index.ejs", '!' + "src/ejs/_*.ejs"])
+//     .pipe(ejs())
+//     .pipe(rename("index.html"))
+//     .pipe( gulp.dest( "./dist" ) );
+// }
+const lookEjs = () => {
+  return gulp.src(["src/ejs/look.ejs", '!' + "src/ejs/_*.ejs"])
+    .pipe(ejs())
+    .pipe(rename("look.html"))
+    .pipe( gulp.dest( "./dist" ) );
+}
+const itemEjs = () => {
+  return gulp.src(["src/ejs/item.ejs", '!' + "src/ejs/_*.ejs"])
+    .pipe(ejs())
+    .pipe(rename("item.html"))
+    .pipe( gulp.dest( "./dist" ) );
+}
+
+const watchFiles = () => {
+  gulp.watch( "src/sass/*.scss", gulp.series( [sassComp, browserSyncReload] ) );
+  gulp.watch( "src/ejs/*.ejs", gulp.series( [lookEjs, itemEjs, browserSyncReload] ) );
+  gulp.watch( "src/js/*.js", gulp.series( [compJs, browserSyncReload] ) );
+}
+
+exports.default = gulp.parallel(watchFiles, browserSyncFunc)
---
+++ src/js/index.js
@@ -0,0 +1,133 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  setTimeout(() => {
+    document.querySelector('body').classList.add('loaded')    
+  }, 500);
+  
+  if (document.querySelector('.js-toTop')) {
+    const topBtn = document.querySelector('.js-toTop');
+    topBtn.addEventListener('click', () => {
+      window.scroll({ top: 0, behavior: 'smooth'})
+    })
+  }
+
+  const fadeinTarget = document.querySelectorAll('.fadein')
+
+  const fadeinCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && !entry.target.classList.contains('show')) {
+        entry.target.classList.add('show')
+      }
+    })
+  }
+
+  const observer = new IntersectionObserver(fadeinCallback, {
+    rootMargin: '-20% 0px',
+  });
+
+  fadeinTarget.forEach(target => {
+    observer.observe(target);
+  });
+
+  const fadeinCreditTarget = document.querySelectorAll('.fadeinCredit')
+  
+  const fadeinCreditCallback = (entries) => {
+    entries.forEach((entry) => {
+      let time = 150
+      if (entry.isIntersecting && !entry.target.classList.contains('show')) {
+        setTimeout(() => {
+          entry.target.querySelectorAll('.fadeinCreditTarget').forEach((item) => {
+            setTimeout(() => {
+              item.classList.add('show')
+            }, time);
+            time += 150
+          })
+        }, 500);
+      }
+    })
+  }
+
+  const creditObserver = new IntersectionObserver(fadeinCreditCallback, {
+    rootMargin: '-8% 0px',
+  });
+
+  fadeinCreditTarget.forEach(target => {
+    creditObserver.observe(target);
+  });
+
+  if (document.querySelector('.js-ModalBtn')) {
+    document.querySelectorAll('.js-ModalBtn').forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        const modal = document.querySelector('.common-Modal')
+        modal.classList.add('active')
+        console.log(e.target);
+        modal.querySelector('.js-Modal-Num').innerHTML = e.target.dataset.num
+        modal.querySelector('.js-Modal-Brand').innerHTML = e.target.dataset.brand;
+        document.querySelector('html').style.overflow = 'hidden'
+      })
+    })
+
+    document.querySelector('.js-Modal-Close').addEventListener('click', () => {
+      document.querySelector('.common-Modal').classList.remove('active')
+      document.querySelector('html').style.overflow = ''
+    })
+
+    document.querySelector('.common-Modal-Bg').addEventListener('click', () => {
+      document.querySelector('.common-Modal').classList.remove('active')
+      document.querySelector('html').style.overflow = ''
+    })
+  }
+
+  class alignWidth {
+    align(target, standardNum, changeNums) {
+      setTimeout(() => {
+        const standardWidth = target.querySelectorAll('.js-alignWidth-standard p')[standardNum].clientWidth
+        changeNums.forEach((num) => {
+          target.querySelectorAll('.js-alignWidth-standard p')[num].style.width = `${standardWidth}px`
+        })
+      }, 400);
+    }
+    alignMulti(target, standardNumMulti, changeNumsMulti) {
+      setTimeout(() => {
+        standardNumMulti.forEach((standardNum, index) => {
+          const standardWidth = target.querySelectorAll('.js-alignWidth-standard p')[standardNum].clientWidth
+          changeNumsMulti[index].forEach((changeNum) => {
+            target.querySelectorAll('.js-alignWidth-standard p')[changeNum].style.width = `${standardWidth}px`
+          })
+        })
+      }, 400);
+    }
+  }
+
+  const aligns = () => {
+    if (window.innerWidth > 767) {
+      if (document.querySelector('.js-look-alignWidth-0')) {
+        new alignWidth().align(document.querySelector('.js-look-alignWidth-0'), 1, [2, 3, 4]);
+        new alignWidth().alignMulti(document.querySelector('.js-look-alignWidth-1'), [0, 1], [[3], [2]]);
+        new alignWidth().align(document.querySelector('.js-look-alignWidth-2'), 4, [0, 1, 2]);
+        new alignWidth().align(document.querySelector('.js-look-alignWidth-3'), 3, [2]);
+        new alignWidth().align(document.querySelector('.js-look-alignWidth-4'), 2, [1]);
+        new alignWidth().align(document.querySelector('.js-look-alignWidth-5'), 0, [1]);
+        new alignWidth().align(document.querySelector('.js-look-alignWidth-6'), 0, [1]);
+        new alignWidth().align(document.querySelector('.js-look-alignWidth-7'), 0, [1]);
+        new alignWidth().align(document.querySelector('.js-look-alignWidth-10'), 3, [1]);
+        new alignWidth().align(document.querySelector('.js-look-alignWidth-11'), 1, [0, 2, 4]);
+      } else if (document.querySelector('.js-item-alignWidth-0')) {
+        new alignWidth().align(document.querySelector('.js-item-alignWidth-2'), 1, [0]);
+        new alignWidth().align(document.querySelector('.js-item-alignWidth-5'), 2, [1]);
+        new alignWidth().align(document.querySelector('.js-item-alignWidth-7'), 2, [0, 1]);
+        new alignWidth().align(document.querySelector('.js-item-alignWidth-8'), 2, [1]);
+      }
+    } else {
+      document.querySelectorAll('.js-alignWidth-standard p').forEach((item) => {
+        item.style.width = ''
+      })
+    }
+  }
+
+  aligns()
+
+  window.addEventListener('resize', () => {
+    aligns()
+  })
+})
